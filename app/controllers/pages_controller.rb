0d106677c55b0ea26b3502bf07e274869f4e1e61
Ben Shyong <likeachild@gmail.com>
2012-10-31 18:09:22 -0400

    imported abbrev data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,15 +3,16 @@ class PagesController < ApplicationController
   end
 
   def process_input
+
 	unless params[:input].blank?
 		@input = params[:input]
 		lines = Array.new
 		@input.lines.each do |l|
 			lines << l
 		end
-		@reporter_number = lines[0]
-		@petitioner = lines[2].split(",")[0]
-		@respondent = lines[4].split()[1]
+		@reporter_number = lines[0] rescue 'Error occurred.'
+		@petitioner = lines[2].split(",")[0] rescue 'Error occurred.'
+		@respondent = lines[4].split()[1] rescue 'Error occurred.'
 	end
   end
 
--- app/models/common_abbreviation.rb
+++ app/models/common_abbreviation.rb
@@ -1,2 +1,5 @@
 class CommonAbbreviation < ActiveRecord::Base
+
+    validates_uniqueness_of :word, :case_sensitive => false
+
 end
