14fe55aa1cbf52258237805eccd3316add49d939
Ben Shyong <likeachild@gmail.com>
2012-10-31 19:32:29 -0400

    initial test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+
   def home
   end
 
@@ -10,10 +11,30 @@ class PagesController < ApplicationController
 		@input.lines.each do |l|
 			lines << l
 		end
+        @decision_year = /(decided).+\d{4}/i.match(@input) {|m| /\d{4}/.match(m.to_s)}
 		@reporter_number = lines[0] rescue 'Error occurred.'
-		@petitioner = lines[2].split(",")[0] rescue 'Error occurred.'
-		@respondent = lines[4].split()[1] rescue 'Error occurred.'
+		petitioner = lines[2].split(",")[0] rescue 'Error occurred.'
+        @petitioner = abbreviate_words(petitioner).titleize
+		respondent = lines[4].split()[1] rescue 'Error occurred.'
+        @respondent = abbreviate_words(respondent).titleize
+        @output = @petitioner.strip.titleize + ' v. ' + @respondent.strip.titleize + ", " + @reporter_number + " (#{@decision_year})."
 	end
   end
+
+private
+
+def abbreviate_words(input = nil)
+    return false if input.nil?
+    result = String.new
+    input.scan(/[\w']+/) do |word|
+        ca = CommonAbbreviation.find_by_word(word.downcase)
+        if ca.blank?
+            result += (word + ' ')
+        else
+            result += (ca.abbreviation + ' ')
+        end
+    end
+    return result
+end
 
 end
