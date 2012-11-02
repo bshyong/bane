83b0440a8fb0c8a42c9f92c7b0983b7f1a41cbf0
Ben Shyong <likeachild@gmail.com>
2012-11-01 20:12:31 -0400

    fix indentation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,21 +4,20 @@ class PagesController < ApplicationController
   end
 
   def process_input
-
-	unless params[:input].blank?
-		@input = params[:input]
-		lines = Array.new
-		@input.lines.each do |l|
-			lines << l
-		end
-        @decision_year = /(decided).+\d{4}/i.match(@input) {|m| /\d{4}/.match(m.to_s)}
-		@reporter_number = lines[0] rescue 'Error occurred.'
-		petitioner = lines[2].split(",")[0] rescue 'Error occurred.'
-        @petitioner = abbreviate_words(petitioner).titleize
-		respondent = lines[4].split()[1] rescue 'Error occurred.'
-        @respondent = abbreviate_words(respondent).titleize
-        @output = @petitioner.strip.titleize + ' v. ' + @respondent.strip.titleize + ", " + @reporter_number + " (#{@decision_year})."
-	end
+  	unless params[:input].blank?
+  		@input = params[:input]
+  		lines = Array.new
+  		@input.lines.each do |l|
+  			lines << l
+  		end
+      @decision_year = /(decided).+\d{4}/i.match(@input) {|m| /\d{4}/.match(m.to_s)}
+  		@reporter_number = lines[0] rescue 'Error occurred.'
+  		petitioner = lines[2].split(",")[0] rescue 'Error occurred.'
+      @petitioner = abbreviate_words(petitioner).titleize
+  		respondent = lines[4].split()[1] rescue 'Error occurred.'
+      @respondent = abbreviate_words(respondent).titleize
+      @output = @petitioner.strip.titleize + ' v. ' + @respondent.strip.titleize + ", " + @reporter_number + " (#{@decision_year})."
+  	end
   end
 
 private
